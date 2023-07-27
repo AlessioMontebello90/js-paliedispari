a069f44213424c981394d538449e7ac7dabf0d9b
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-07-27 18:01:37 +0200

    add function JS Palindromi ex

--- palindromi/JS/main.js
+++ palindromi/JS/main.js
@@ -1,3 +1,12 @@
 // chiedo all'utente una parola che andra inserita nella variabile stabilita al riconoscimento
 
 const userWord = prompt("choose a word");
+
+const wordL = userWord.length - 1;
+
+function read(val) {
+  for (let i = wordL; i >= 0; i--) {
+    readWord += val[i];
+  }
+  return readWord;
+}
