fc064b74fede2367483beab8cec97ccaf7b54bf6
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-07-27 18:14:45 +0200

    fix cicle JS palindromi ex

--- palindromi/JS/main.js
+++ palindromi/JS/main.js
@@ -3,5 +3,23 @@
 const userWord = prompt("choose a word");
 
 const wordL = userWord.length - 1;
+let readWord = "";
 
-// generiamo una funzione che riconosca
+// generiamo una funziona che legga la parola inserita dall'utente
+
+function read(val) {
+  for (let i = wordL; i >= 0; i--) {
+    readWord += val[i];
+  }
+  return readWord;
+}
+
+// generiamo un ciclo IF che riconosca la palindromia della parola inserita dall'utente
+
+const reversedWord = read(userWord);
+
+if (userWord == reversedWord) {
+  alert("A te, o poeta!!!!");
+} else {
+  alert("try again!!!!");
+}
